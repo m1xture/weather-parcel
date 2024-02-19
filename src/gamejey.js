dff1c67cdfe9d05a62063f57142b5a07aecb4da8
m1xture <109725502+m1xture@users.noreply.github.com>
2024-02-19 17:25:06 +0200

    sml

--- src/gamejey.js
+++ src/gamejey.js
@@ -44,10 +44,6 @@ document.addEventListener("keydown", (evt) => {
 const myStack = new PNotify.Stack({ dir1: "down", firstpos1: 25 });
 
 function sendLoseAlert() {
-  //   PNotify.notice({
-  //     text: `lflflf by me`,
-  //     stack: myStack,
-  //   });
   PNotify.error({
     title: "Lose!",
     text: "",
@@ -66,3 +62,56 @@ function renderText() {
   textElem.textContent = arrayOfKeys[currIndex];
 }
 renderText();
+
+//todo: #2
+
+const canvaEl = document.getElementById("sales-chart");
+
+const chartData = {
+  labels: [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+    "29",
+    "30",
+  ],
+  datasets: [
+    {
+      label: "Продажі за останній місяць",
+      data: [
+        150, 220, 180, 200, 250, 300, 280, 350, 400, 380, 420, 450, 500, 550,
+        600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200,
+        1250, 1300, 1350,
+      ],
+      backgroundColor: "#2196f3",
+      borderColor: "#2196f3",
+      borderWidth: 1,
+    },
+  ],
+};
+const salesChart = new Chart(canvaEl, { type: "line", data: chartData });
